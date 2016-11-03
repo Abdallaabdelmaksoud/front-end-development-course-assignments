5a6201150a5e6e82382385eb79b80db02a270d09
Abdallaabdelmaksoud <abdalla.abdelmaksoud@outlook.com>
2016-11-03 15:53:44 -0500

    all done

---
+++ Assignment8/JS-Assignment/scripts/main.js
@@ -0,0 +1,24 @@
+
+
+var myName;
+myName = "Abdalla Abdelmaksoud";
+
+document.getElementById('name').innerHTML = myName;
+
+
+var nunmber;
+number = 100;
+document.getElementById('number').innerHTML = number
+
+
+ var day;
+ day = Date();
+ console.log(day);
+
+var coffee;
+coffee =" number of cups of coffee you have had today?";
+console.log(coffee);
+
+var cloth;
+cloth = " are you wearing green?";
+console.log(cloth);
