952f55b175fe439e110202d1fb842b5f1be55014
Abdallaabdelmaksoud <abdalla.abdelmaksoud@outlook.com>
2016-11-30 19:07:43 -0600

    not done yet

--- Assignment15/scripts/main.js
+++ Assignment15/scripts/main.js
@@ -2,9 +2,28 @@ $(document).ready(function(){
 
      ///// Code goes here
 
+$("#btnOne").click(function(){
+    alert  ("You CLicked Me");
+    
+});
 
 
+$("#btnTwo").click(function(){
+
+$(".update-html").replaceWith("<h3>Hello Word!</h3>");
+
+});
+
+$("#btnThree").click(function(){
+
+    $("#text-update").val  ("Hello World")
+
+});
+
+    $("#btnCopyHtml").click(function(){
 
 
+        
+    })
 
 });
