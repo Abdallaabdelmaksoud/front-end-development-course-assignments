58b6d6596d02947d5b7a69adc6152d8a6cf9ca9b
Abdallaabdelmaksoud <abdalla.abdelmaksoud@outlook.com>
2016-12-06 21:02:17 -0600

    all done

--- Assignment16/scripts/main.js
+++ Assignment16/scripts/main.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
 
+        $('#btnShow').click(function(){
+        $('.display-basic').show();
+    });
+        $('#btnHide').click(function(){
+        $('.display-basic').hide();
+    });
+        $('#btnToggle').click(function(){
+        $('.display-basic').toggle();
+    });
 
+        $('#btnFadeOut').click(function(){
+        $('.display-fade').fadeOut('slow');
+    });
 
-});
+        $('#btnFadeIn').click(function(){
+        $('.display-fade').fadeIn('fast');
+    });
+
+        $('#btnFadeToggle').click(function(){
+        $('.display-fade').fadeToggle();
+    });
+
+        $('#btnSlideDown').click(function(){
+        $('.display-slide').slideDown('1000');
+    });
+
+        $('#btnSlideUp').click(function(){
+        $('.display-slide').slideUp('500');
+    });
+
+        $('#btnSlideToggle').click(function(){
+        $('.display-slide').slideToggle();
+    });
+    });
